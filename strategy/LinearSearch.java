b77a233c2efe5857029ac6a6529e5c26f5ef1e49
Kenny Phan <kenobusiness1@gmail.com>
2021-09-03 22:08:55 -0400

    Created observer folder, added bookdriver file

--- strategy/LinearSearch.java
+++ strategy/LinearSearch.java
@@ -7,7 +7,10 @@ public class LinearSearch implements SearchBehavior
     {
 
     }
-
+    /**
+     * Iterates from the start of the array to the end of the array one by one and checks to see if the targetted name already exists
+     * in the guest list. Before comparing strings, the toLowerCase method is used due to the Contains method being case-sensitive. 
+     */
     public boolean contains(ArrayList<String> data, String item)
     {
         for(int i = 0; i < data.size(); i++)
