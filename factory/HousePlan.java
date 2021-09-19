d676e5eb24f4c12d71aab08b1249b70831fd5e01
Kenny Phan <kenobusiness1@gmail.com>
2021-09-19 02:50:46 -0400

    added factory folder

---
+++ factory/HousePlan.java
@@ -0,0 +1,45 @@
+public class HousePlan {
+    protected ArrayList<String> materials;
+    protected ArrayList<String> features;
+    private int numRooms;
+    private int numWindows;
+    private int squareFeet;
+    public HousePlan(int numRooms, int numWindows, int squareFeet)
+    {
+
+
+    }
+    protected void setMaterials()
+    {
+
+
+    }
+    protected void setFeatures()
+    {
+
+    }
+    public ArrayList<String> getMaterials()
+    {
+
+    }
+    public ArrayList<String> getFeatures()
+    {
+
+    }
+    public int getNumRooms()
+    {
+
+    }
+    public int getNumWindows()
+    {
+
+    }
+    public int getSquareFeet()
+    {
+
+    }
+    public String toString()
+    {
+        
+    }
+}
